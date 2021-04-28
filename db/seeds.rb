fc88deb66c5496e5b511ff88cf6cdb58eeb95d70
Rahman Shafiee <r.shafiee@gmx.de>
2021-04-29 01:06:45 +0300

    Added cardselement in the home and index as well as did made the navbar sticky

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 puts 'test'
+
 vanmoof = Bike.create(bike_name: 'VanMoof S3', bike_address: 'Berlin', bike_description: 'Very good bike', price: 20, availability: true, bike_url: 'www.test.de', user_id: 1)
 vanmoof.save!
 cowboy = Bike.create(bike_name: 'Cowboy S3', bike_address: 'Brussels', bike_description: 'A stable bike', price: 19, availability: true, bike_url: 'www.test.de', user_id: 1)
