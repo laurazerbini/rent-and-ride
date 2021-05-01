6107c58d33450cb59b8b472e4b72cdc517c862a0
Rahman Shafiee <r.shafiee@gmx.de>
2021-05-01 14:13:57 +0300

    A user can now create a booking, display his current and previous bookings.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :bikes do
-    resources :bookings, only: [:index, :show, :new, :create ]
+    resources :bookings, only: [ :new, :create ]
   end
+  resources :bookings, only: [ :index, :show ]
 end
