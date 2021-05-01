0f5fbd58c4291657feaa9abc526ebf62cd65dc9b
Rahman Shafiee <r.shafiee@gmx.de>
2021-05-01 15:21:55 +0300

    Added .env file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_30_205854) do
+ActiveRecord::Schema.define(version: 2021_05_01_091828) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,7 +73,6 @@ ActiveRecord::Schema.define(version: 2021_04_30_205854) do
     t.string "first_name"
     t.string "last_name"
     t.string "city"
-    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
